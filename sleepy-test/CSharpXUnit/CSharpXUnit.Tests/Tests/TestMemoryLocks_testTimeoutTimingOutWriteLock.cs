4be97704ef5c71585005501885390b17c331bd5c
Publio Blenilio <publio.blenilio@gmail.com>
2025-02-15 12:34:22 -0300

    refactor: update tests

--- sleepy-test/CSharpXUnit/CSharpXUnit.Tests/Tests/TestMemoryLocks_testTimeoutTimingOutWriteLock.cs
+++ sleepy-test/CSharpXUnit/CSharpXUnit.Tests/Tests/TestMemoryLocks_testTimeoutTimingOutWriteLock.cs
@@ -1,20 +1,20 @@
 public class TestMemoryLocks
 {
     [Fact]
-    public Task TestTimeoutTimingOutWriteLock()
+    public void TestTimeoutTimingOutWriteLock()
     {
         StringBuilder sb = new StringBuilder("");
         Locker l1 = new WriteLocker("a", 1, 0, sb);
         Locker l2 = new WriteLocker("a", 2, 50, sb);
 
-        var t1 = Task.Run(() => l1.Start());
-        Task.Sleep(500);
-        var t2 = Task.Run(() => l2.Start());
-        Task.Sleep(500);
+        new Thread(l1.Run).Start();
+        Thread.Sleep(500);
+        new Thread(l2.Run).Start();
+        Thread.Sleep(500);
         l1.Finish();
-        Task.Sleep(500);
+        Thread.Sleep(500);
         l2.Finish();
-        Task.Sleep(500);
+        Thread.Sleep(500);
         Assert.Equal("a:1-L a:2-N a:1-U", sb.ToString().Trim());
     }
 }
